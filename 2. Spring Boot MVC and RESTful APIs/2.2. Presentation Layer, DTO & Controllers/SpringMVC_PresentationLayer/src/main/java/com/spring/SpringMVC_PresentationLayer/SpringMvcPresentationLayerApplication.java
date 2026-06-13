963e0e5fb013d1bf6598c99ea4f9e59b9ba29d72
AbhishekLayek <abhisheklayek57@gmail.com>
2026-06-13 19:21:20 +0530

    Spring MVC Presentation Layer

---
+++ 2. Spring Boot MVC and RESTful APIs/2.2. Presentation Layer, DTO & Controllers/SpringMVC_PresentationLayer/src/main/java/com/spring/SpringMVC_PresentationLayer/SpringMvcPresentationLayerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.SpringMVC_PresentationLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPresentationLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcPresentationLayerApplication.class, args);
+	}
+
+}
---
+++ 2. Spring Boot MVC and RESTful APIs/2.2. Presentation Layer, DTO & Controllers/SpringMVC_PresentationLayer/src/test/java/com/spring/SpringMVC_PresentationLayer/SpringMvcPresentationLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringMVC_PresentationLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPresentationLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
