3dfecbeb5b31b107999800406d64583f0a07a2ec
AbhishekLayek <abhisheklayek57@gmail.com>
2026-06-14 16:51:39 +0530

    Spring MVC Persistence Layer & JPA

---
+++ 2. Spring Boot MVC and RESTful APIs/2.3. Persistence Layer & JPA/SpringMVC_PersistenceLayer/src/main/java/com/spring/SpringMVC_PersistenceLayer/SpringMvcPersistenceLayerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.SpringMVC_PersistenceLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPersistenceLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcPersistenceLayerApplication.class, args);
+	}
+
+}
---
+++ 2. Spring Boot MVC and RESTful APIs/2.3. Persistence Layer & JPA/SpringMVC_PersistenceLayer/src/test/java/com/spring/SpringMVC_PersistenceLayer/SpringMvcPersistenceLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringMVC_PersistenceLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPersistenceLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
