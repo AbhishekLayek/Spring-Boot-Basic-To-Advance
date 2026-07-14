e3e0f26d6eed40ca27fcac2b0b9044e7e48ce59b
AbhishekLayek <abhisheklayek57@gmail.com>
2026-07-14 23:25:25 +0530

    Spring Security

---
+++ 5. Spring Security Fundamentals/5.2. Internal Working of Spring Security Part 1/SecurityApp/src/main/java/com/spring/SecurityApp/configs/AppConfig.java
@@ -0,0 +1,24 @@
+package com.spring.SecurityApp.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.spring.SecurityApp.auth.AuditorAwareImpl;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "getAuditorAwareObject")
+public class AppConfig {
+	
+	@Bean
+	ModelMapper getModelMapperObject() {
+		return new ModelMapper();
+	}
+	
+	@Bean
+	AuditorAware<String> getAuditorAwareObject(){
+		return new AuditorAwareImpl();
+	}
+}
