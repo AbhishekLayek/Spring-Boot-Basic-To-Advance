7e6b455be3ab6402bd0058613ca7ed24332bb087
AbhishekLayek <abhisheklayek57@gmail.com>
2026-08-06 23:42:43 +0530

    User Session Management With JWT

--- 6. Spring Security Advanced/6.3. User Session Management With JWT/SignupLogin/src/main/java/com/spring/SignupLogin/services/AuthService.java
+++ 6. Spring Security Advanced/6.3. User Session Management With JWT/SignupLogin/src/main/java/com/spring/SignupLogin/services/AuthService.java
@@ -26,6 +26,7 @@ public class AuthService {
 	private final AuthenticationManager authenticationManager;
 	private final JWTService jwtService;
 	private final UserService userService;
+	private final SessionService sessionService;
 	
 	public UserDTO signup(SignupDTO signupDTO) {
 		UserEntity userEntity = modelMapper.map(signupDTO, UserEntity.class);
@@ -45,12 +46,17 @@ public class AuthService {
 		String accessToken = jwtService.generateAccessToken(user);
 		String refreshToken = jwtService.generateRefreshToken(user);
 		
+		sessionService.createSession(user, refreshToken);
+		
 		return new LoginResponse(user.getId(), accessToken, refreshToken);
 	}
 
 	public LoginResponse refresh(String refreshToken) {
 		
 		Long userId = jwtService.getUserIdFromToken(refreshToken);
+		
+		sessionService.validateSession(refreshToken);
+		
 		UserEntity user = userService.getUserById(userId);
 		
 		String accessToken = jwtService.generateAccessToken(user);
