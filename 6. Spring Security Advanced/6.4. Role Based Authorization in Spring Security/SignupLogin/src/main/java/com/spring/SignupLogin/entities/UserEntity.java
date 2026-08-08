90409835f1b44c27d207ec141e2b5bf66d2c5979
AbhishekLayek <abhisheklayek57@gmail.com>
2026-08-08 22:18:04 +0530

    User Session Management With JWT

---
+++ 6. Spring Security Advanced/6.4. Role Based Authorization in Spring Security/SignupLogin/src/main/java/com/spring/SignupLogin/entities/UserEntity.java
@@ -0,0 +1,56 @@
+package com.spring.SignupLogin.entities;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+@Builder
+public class UserEntity implements UserDetails{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+	private String name;
+	
+	@Column(nullable = false,unique = true, length = 50)
+	private String email;
+	
+	private String password;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.emptyList();
+	}
+	
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public @Nullable String getPassword() {
+		return this.password;
+	}
+}
