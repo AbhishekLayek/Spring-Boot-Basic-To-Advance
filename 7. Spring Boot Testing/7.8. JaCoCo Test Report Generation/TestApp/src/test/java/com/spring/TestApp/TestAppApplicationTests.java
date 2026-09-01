c33a761888e0762c803982cee42d9bf6a88b0768
AbhishekLayek <abhisheklayek57@gmail.com>
2026-09-01 18:04:02 +0530

    JaCoCo Test Report Generation

--- 7. Spring Boot Testing/7.8. JaCoCo Test Report Generation/TestApp/src/test/java/com/spring/TestApp/TestAppApplicationTests.java
+++ 7. Spring Boot Testing/7.8. JaCoCo Test Report Generation/TestApp/src/test/java/com/spring/TestApp/TestAppApplicationTests.java
@@ -2,8 +2,12 @@ package com.spring.TestApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import com.spring.TestApp.configs.TestContainerConfig;
 
 @SpringBootTest
+@Import(TestContainerConfig.class)
 class TestAppApplicationTests {
 
 	@Test
